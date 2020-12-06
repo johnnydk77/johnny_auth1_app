993c683ae3010b2c76e8e13bb145044af95d5e63
Johnny Kinnaird <johnnydk77@gmail.com>
2020-12-06 15:59:50 -0500

    paths established

--- src/App.js
+++ src/App.js
@@ -1,12 +1,25 @@
-import logo from './logo.svg';
+import React from 'react';
 import './App.css';
+import Home from './Components/Home';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom';
 
-function App() {
+
+const App = () => {
+
+
   return (
-    <div className="App">
-      <h2>Hello World!</h2>
-    </div>
+    <Router>
+      <div classname='App'>
+        
+        <Switch>
+          <Route exact path='/' component={Home}/>
+
+        </Switch>
+      </div>
+    </Router>
+         
   );
-}
+};
+
 
 export default App;
---
+++ src/Components/Home.jsx
@@ -0,0 +1,15 @@
+import React, { Component } from 'react';
+// import { BrowserRouter as Router } from 'react-router-dom';
+
+
+class Home extends Component {
+    
+    render() {
+        return(
+            <h2>Pizza 42!!!</h2>
+        )
+    }
+}
+
+
+export default Home;
